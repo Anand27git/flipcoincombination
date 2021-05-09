0d16317bdbef59bbce726c4a93755896818d38cf
Anand27git <anandkumar256k@gmail.com>
2021-05-09 11:08:00 +0530

    commit success

--- flipcom.sh
+++ flipcom.sh
@@ -2,3 +2,12 @@
 
 echo "Welcome to the flip combination"
 
+Head=1;
+Flip_Check=$((RANDOM%2))
+Num_Of_Flips=5
+if [ $Flip_Check -eq $Head ]
+then
+	echo " coin is HEADS"
+else
+	echo " coin is TAILS"
+fi
