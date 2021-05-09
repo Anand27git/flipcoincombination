054d54579af5830445828e2be36709f30b2bf503
Anand27git <anandkumar256k@gmail.com>
2021-05-09 11:48:13 +0530

    commit success

--- flipcom.sh
+++ flipcom.sh
@@ -11,3 +11,28 @@ then
 else
 	echo " coin is TAILS"
 fi
+
+
+#uc-2
+headCount=0
+tailCount=0
+while [[ $headCount -le 11 && $tailCount -le 11 ]];
+do
+a=$((RANDOM%2))
+if [[ $a -eq 0 ]]
+then
+headCount=$((headCount+1))
+else
+tailCount=$(( tailCount+1 ))
+fi
+done
+declare -a coin
+if [[ $headCount -eq 11 ]]
+then
+echo "Head Count" $headcount
+else
+echo "Tail Count" $tailCount
+
+coin=( [0]=$headCount [1]=$tailCount )
+echo "stored:${coin[@]}"
+fi
